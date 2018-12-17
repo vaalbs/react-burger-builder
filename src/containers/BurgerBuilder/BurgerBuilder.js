06516ecc8c9fcba0d673960b95afba4cc2256b6b
Valeria Almeida de Oliveira <valeriaalmeidadeoliveira@gmail.com>
2018-12-17 20:33:41 -0300

    Navigating to the Checkout Page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,28 +91,30 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Valéria Almeida de Oliveira',
-                address: {
-                    street: 'Teststreet, 1',
-                    zipCode: '53452',
-                    country: 'Brazil'
-                },
-                email: 'vaalbs@teste.com.br'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading:false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading:false, purchasing: false});
-            });
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Valéria Almeida de Oliveira',
+        //         address: {
+        //             street: 'Teststreet, 1',
+        //             zipCode: '53452',
+        //             country: 'Brazil'
+        //         },
+        //         email: 'vaalbs@teste.com.br'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading:false, purchasing: false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading:false, purchasing: false});
+        //     });
+
+        this.props.history.push('/checkout');
     }
 
     render() {

06a6b6fb356048077b907f262c3bfcfe802cb6fc
Valeria Almeida de Oliveira <valeriaalmeidadeoliveira@gmail.com>
2018-12-23 12:39:39 -0300

    Order Subimission & Passing Data between pages

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,35 +90,13 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continue!');
-        // this.setState({loading: true});
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Valéria Almeida de Oliveira',
-        //         address: {
-        //             street: 'Teststreet, 1',
-        //             zipCode: '53452',
-        //             country: 'Brazil'
-        //         },
-        //         email: 'vaalbs@teste.com.br'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({loading:false, purchasing: false});
-        //     })
-        //     .catch(error => {
-        //         this.setState({loading:false, purchasing: false});
-        //     });
-
         const queryParams = [];
 
         for(let i in this.state.ingredients){
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+
+        queryParams.push('price=' + this.state.totalPrice);
 
         const queryString = queryParams.join('&');
 

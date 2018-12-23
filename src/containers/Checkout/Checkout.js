b76344f1cbe550fe4162ba7d8f761e7fb69224b7
Valeria Almeida de Oliveira <valeriaalmeidadeoliveira@gmail.com>
2018-12-23 12:04:36 -0300

    Navigating to the Contact Data Component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
@@ -37,6 +39,9 @@ class Checkout extends Component {
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}
                     ingredients={this.state.ingredients} />
+                <Route 
+                    path={this.props.match.path + '/contact-data'} 
+                    component={ContactData}/>
             </div>
         );
     }

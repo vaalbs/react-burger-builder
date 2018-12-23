4a3b1816bd2263b80a3a47766e74ae68c70471fc
Valeria Almeida de Oliveira <valeriaalmeidadeoliveira@gmail.com>
2018-12-23 14:32:08 -0300

    Implementing Navigation Links

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import Order from './containers/Orders/Orders';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   render() {
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -6,6 +6,7 @@ class Orders extends Component {
         return (
             <div>
                 <Order />
+                <Order />
             </div>
         );
     }

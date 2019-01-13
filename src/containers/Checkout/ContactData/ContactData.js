b4ef2ff9447a293a5da9bbf10a293c5f882f5028
Valeria Almeida de Oliveira <valeriaalmeidadeoliveira@gmail.com>
2019-01-12 21:44:32 -0300

    The Order Reducer

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -91,8 +91,7 @@ class ContactData extends Component {
                 valid: true
             }
         },
-        formIsValid: false,
-        loading: false
+        formIsValid: false
     }
 
     orderHandler = (event) => {
@@ -179,7 +178,7 @@ class ContactData extends Component {
             </form>
         );
 
-        if(this.state.loading){
+        if(this.props.loading){
             form = <Spinner />;
         }
 
@@ -195,12 +194,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        loading: state.loading
     }
 };
 
 const mapDipatchToProps = dispatch => {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBugerStart(orderData))
+    return {
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+    };
 };
 
 export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));

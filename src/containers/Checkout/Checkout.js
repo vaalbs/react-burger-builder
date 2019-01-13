c4846f2fb8f3ad59e2472df340e4c85ae8606a50
Valeria Almeida de Oliveira <valeriaalmeidadeoliveira@gmail.com>
2019-01-13 12:24:04 -0300

    Handling Purchases & Updating the UI

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -17,8 +17,11 @@ class Checkout extends Component {
     render(){
         let summary = <Redirect to="/" />;
         if (this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? <Redirect to="/" /> : null;
+
             summary = (
                 <div>
+                    {purchasedRedirect}
                     <CheckoutSummary 
                         checkoutCancelled={this.checkoutCancelledHandler}
                         checkoutContinued={this.checkoutContinuedHandler}
@@ -35,7 +38,8 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     }
 };
 

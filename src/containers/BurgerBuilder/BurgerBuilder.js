8d476d62803e8233eee94602456d8e64095473e9
Valeria Almeida de Oliveira <valeriaalmeidadeoliveira@gmail.com>
2019-02-03 11:23:25 -0300

    Forwarding Unauthenticated Users

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,7 +32,11 @@ class BurgerBuilder extends Component {
     }
 
     purchaseHandler = () => {
-        this.setState({purchasing: true});
+        if(this.props.isAuthenticated){
+            this.setState({purchasing: true});
+        } else {
+            this.props.history.push('/auth');
+        }
     }
 
     purchaseCancelHandler = () => {
@@ -66,7 +70,8 @@ class BurgerBuilder extends Component {
                         disabled={disabledInfo}
                         purchasable = {this.updatePurchaseState(this.props.ings)}
                         price={this.props.price}
-                        ordered={this.purchaseHandler}/>
+                        ordered={this.purchaseHandler}
+                        isAuth={this.props.isAuthenticated}/>
                 </Aux>
             );
             orderSummary = <OrderSummary 
@@ -93,7 +98,8 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null
     };
 }
 

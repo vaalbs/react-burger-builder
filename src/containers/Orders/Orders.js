6a40c4a2ee0d4320cf252e7cc6a4877110de7845
Valeria Almeida de Oliveira <valeriaalmeidadeoliveira@gmail.com>
2019-02-02 22:50:50 -0300

    Acessing Protected Resources

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,18 +9,18 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 class Orders extends Component {
 
     componentDidMount(){
-        this.props.onFetchOrders();
+        this.props.onFetchOrders(this.props.token);
     }
 
     render() {
         let orders = <Spinner />;
         if (!this.props.loading) {
             orders = this.props.orders.map(order => (
-                    <Order 
-                        key={order.id}
-                        ingredients={order.ingredients}
-                        price={order.price} />
-                ))
+                <Order 
+                    key={order.id}
+                    ingredients={order.ingredients}
+                    price={order.price} />
+            ));
         }
         return (
             <div>
@@ -33,13 +33,14 @@ class Orders extends Component {
 const mapStateToProps = state => {
     return {
         orders: state.order.orders,
-        loading: state.order.loading
+        loading: state.order.loading,
+        token: state.auth.token
     };
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrders: () => dispatch(actions.fetchOrders())
+        onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
     }
 }
 

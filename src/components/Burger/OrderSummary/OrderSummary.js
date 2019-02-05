7a29e9d936e0f486be6a511c21953491a225a1c1
Valeria Almeida de Oliveira <valeriaalmeidadeoliveira@gmail.com>
2019-02-04 21:27:10 -0300

    Removing console.log()s

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,10 +4,6 @@ import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
 
-    componentWillUpdate() {
-        console.log('[OrderSummary] WillUpdate');
-    }
-
     render (){
 
         const ingredientSummary = Object.keys(this.props.ingredients)
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 import thunk from 'redux-thunk';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ?window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  : null || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  : null || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,

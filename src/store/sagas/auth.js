6dab142a94b43523f51dd65f17b405c61b4dc98e
Valeria Almeida de Oliveira <valeriaalmeidadeoliveira@gmail.com>
2019-02-13 21:52:21 -0300

    Diving deeper into Sagas

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,12 +1,12 @@
-import { put, delay } from 'redux-saga/effects';
+import { put, delay, call } from 'redux-saga/effects';
 import axios from 'axios';
 
 import * as actions from '../actions/index';
 
 export function* logoutSaga(action) {
-    yield localStorage.removeItem('token');
-    yield localStorage.removeItem('expirationDate');   
-    yield localStorage.removeItem('userId'); 
+    yield call([localStorage, 'removeItem'], "token");
+    yield call([localStorage, 'removeItem'], "expirationDate");
+    yield call([localStorage, 'removeItem'], "userId");
     yield put(actions.logoutSucced());
 };
 
@@ -43,16 +43,16 @@ export function* authUserSaga(action) {
 
 export function* authCheckStateSaga(action) {
     const token = yield localStorage.getItem('token');
-        if(!token){
+    if(!token){
+        yield put(actions.logout());
+    } else {
+        const expirationDate = yield new Date(localStorage.getItem('expirationDate'));
+        if(expirationDate <= new Date()){
             yield put(actions.logout());
         } else {
-            const expirationDate = yield new Date(localStorage.getItem('expirationDate'));
-            if(expirationDate <= new Date()){
-                yield put(actions.logout());
-            } else {
-                const userId = yield localStorage.getItem('userId');
-                yield put(actions.authSuccess(token, userId));   
-                yield put(actions.checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));                    
-            }
+            const userId = yield localStorage.getItem('userId');
+            yield put(actions.authSuccess(token, userId));   
+            yield put(actions.checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));                    
         }
+    }
 };
--- src/store/sagas/order.js
+++ src/store/sagas/order.js
@@ -6,7 +6,7 @@ import * as actions from '../actions/index';
 export function* purchaseBurgerSaga(action) {
     yield put(actions.purchaseBurgerStart());
     try {
-        const response = yield axios.post('/orders.json?auth=' + action.token, action.orderData);
+        const response = yield axios.post("/orders.json?auth=" + action.token, action.orderData);
         yield put(actions.purchaseBurgerSuccess(response.data.name, action.orderData));
     } catch(error) {
         yield put(actions.purchaseBurgerFail(error));
@@ -15,9 +15,9 @@ export function* purchaseBurgerSaga(action) {
 
 export function* fetchOrdersSaga(action) {
     yield put(actions.fetchOrdersStart());
-    const queryParams = '?auth=' + action.token + '&orderBy="userId"&equalTo="' + action.userId + '"';
+    const queryParams = "?auth=" + action.token + '&orderBy="userId"&equalTo="' + action.userId + '"';
     try {
-        const response = yield axios.get('/orders.json' + queryParams);
+        const response = yield axios.get("/orders.json" + queryParams);
         const fetchedOrders = [];
         for(let key in response.data){
             fetchedOrders.push({
